2e7167ce4ca98ed4a43e032bfc3ea1a3b5db147b
unknown <inprapoe@gmail.com>
2021-06-29 12:06:02 +0700

    tambah halaman payment

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,17 +1,27 @@
+
+const userNav = document.querySelector('#userNav');    
+const navOverlay = document.querySelector('#navOverlay');  
+
 function toggleNav() {
     let myNav = document.getElementById("myNav");
     myNav.style.height = myNav.style.height === '100%' ? '0' : '100%';
+    navOverlay.style.display = 'flex'
 }
 
-window.onscroll = function() {scrollFunction()};
+let landingNav = document.querySelector('.landing-nav')
+let navLogIn = document.querySelector('#navLogIn')
+let pathName = window.location.pathname.replace("/public", '')
+
+
+
+window.onscroll = pathName === '/' ? function() {scrollFunction()} : ''
 function scrollFunction() {
-    let nav = document.querySelector('nav')
-    let navLogIn = document.querySelector('#navLogIn')
+   
     if (document.body.scrollTop > 480 || document.documentElement.scrollTop > 480) {
-        nav.classList.add('scrl-nav')
+        landingNav.classList.add('scrl-nav')
         navLogIn.classList.add('btn-secondary-gold')
     } else {
-        nav.classList.remove('scrl-nav')
+        landingNav.classList.remove('scrl-nav')
         navLogIn.classList.remove('btn-secondary-gold')
     }
 }
@@ -31,4 +41,20 @@ for (i = 0; i < acc.length; i++) {
         } 
         this.children[1].classList.toggle('-rotate-180')
     });
+}
+  
+
+const openUserNav = () => {       
+    if (window.innerWidth >= 768 && userNav.style.display === '') {
+        userNav.style.display = 'flex'
+        navOverlay.style.display = 'flex'
+    } else {
+        closeNav()
+    }
+}
+
+const closeNav = () => {    
+    userNav.style.display = ''   
+    navOverlay.style.display = ''
+    window.innerWidth >= 768 ? '' : myNav.style.height = '0'
 }
